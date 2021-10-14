6ec3b46c6a7e8a1019d7e21ee2531a6253a38795
David-Chae <aksrud859@hanmail.net>
2021-10-14 17:59:19 +1100

    Update script.js
    Add javascript's event listener and logic for opening/closing breakfast menu main dishes and side dishes.

--- js/script.js
+++ js/script.js
@@ -52,3 +52,72 @@ let repeat = function(activeClass){
 }
 repeat();
 
+///////////////////////////////////////////////////////////////
+// Javascript for opening menu items.
+
+const dishs = document.querySelectorAll('.dish')
+
+dishs.forEach(function(dish){
+    dish.addEventListener("click", function(e){
+        e.preventDefault();
+        
+        let dishButton = dish.childNodes[1];
+        let dishText = dishButton.childNodes[1];
+        let dishName = dishText.childNodes[1];
+        let dishIngredients = dishText.childNodes[3];
+        let buttonIcon = dishButton.childNodes[3].childNodes[1];
+        let dishImg = dish.childNodes[3];
+
+        //console.log(buttonIcon);
+
+        if (dishButton.style.backgroundColor === '' || dishButton.style.backgroundColor === '#fff' ||
+        dishButton.style.backgroundColor === 'rgb(255, 255, 255)'){
+              dishButton.style.backgroundColor = '#ff6961';
+              dishName.style.color = '#fff';
+              dishIngredients.style.color = '#fff';
+              dishImg.style.display = "block";
+              buttonIcon.name = "chevron-up-outline";
+              buttonIcon.style.color = "#fff";
+        }else{
+              dishButton.style.backgroundColor = '#fff';
+              dishName.style.color = '#16A085';
+              dishIngredients.style.color = '#16A085';
+              dishImg.style.display = "none";
+              buttonIcon.name = "chevron-down-outline";
+              buttonIcon.style.color = "#16A085";
+        }
+
+    })
+})
+
+const sides = document.querySelectorAll('.side');
+
+sides.forEach(function(side){
+    side.addEventListener("click", function(e){
+        e.preventDefault();
+        
+        let sideButton = side.childNodes[1];
+        let sideText = sideButton.childNodes[1];
+        let sideName = sideText.childNodes[1];
+        let buttonIcon = sideButton.childNodes[3].childNodes[1];
+        let sideImg = side.childNodes[3];
+
+        console.log(side.childNodes);
+
+        if (sideButton.style.backgroundColor === '' || sideButton.style.backgroundColor === '#fff' ||
+        sideButton.style.backgroundColor === 'rgb(255, 255, 255)'){
+              sideButton.style.backgroundColor = '#ff6961';
+              sideName.style.color = '#fff';
+              sideImg.style.display = "block";
+              buttonIcon.name = "chevron-up-outline";
+              buttonIcon.style.color = "#fff";
+        }else{
+              sideButton.style.backgroundColor = '#fff';
+              sideName.style.color = '#16A085';
+              sideImg.style.display = "none";
+              buttonIcon.name = "chevron-down-outline";
+              buttonIcon.style.color = "#16A085";
+        }
+
+    })
+})
